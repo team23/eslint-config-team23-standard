ab5ae371766929aa02abe10d5ebb201aeb1004ff
Patrick Schröter <schroeter@team23.de>
2021-12-15 13:13:40 +0100

    feat(chore): update example files and added more tests

--- src/index.invalid.js
+++ src/index.invalid.js
@@ -1,12 +1,47 @@
 /**
- * a sample function to lint
- *
- * @param value
+    * jsdoc/no-types
+ * @param {string} value
  */
-function testEslintConfig(value) {
-    return value;
+function yoda(value) {
+    return 2 === value;
 }
 
-const magicNumber = 10;
+const value = 'value';
 
-testEslintConfig({ someValue: 100 });
+const preferTemplate = "Hello, " + name + "!";
+const validTypeof = typeof value == "undefimed";
+const magicNumber = {'value': 100};
+const useIsnan = value === NaN;
+const spaceInParens = ( 1 + 2 );
+
+function spaceBeforeFunctionParen (callback) {
+
+};
+
+const radix = parseInt("071");
+const preferObjectSpread = Object.assign({}, magicNumber);
+var noVar = 1;
+let preferConst = 1;
+
+function preferArrowCallback(callback) {}(function(a) { return a; });
+
+function paddingLineBetweenStatements() {
+    const a = 1;
+    return a;
+}
+
+var oneVar, twoVar = 1;
+const objectShorthand = {
+        oneVar: oneVar
+}
+
+
+var noUnneededTernary = twoVar === 1 ? true : false;
+
+(function(){
+    return void 0;
+})();
+
+const templateCurlySpacing = `${noVar}`
+
+([]).filter(a => a).map(a => a);
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,4 @@
 const { ESLint } = require('eslint');
-const config = require('../.eslintrc.js');
 
 async function validate(file) {
     return (await new ESLint().lintFiles([file]))[0];
@@ -24,8 +23,35 @@ describe('Validate ESLint configs', () => {
 
     describe('for rule', () => {
         test.each([
-            ['no-unused-vars'],
-            ['no-magic-numbers'],
+            'quotes',
+            'semi',
+            'no-magic-numbers',
+            'no-shadow',
+            'prefer-arrow/prefer-arrow-functions',
+            'comma-dangle',
+            'eqeqeq',
+            'jsdoc/check-alignment',
+            'jsdoc/newline-after-description',
+            'newline-per-chained-call',
+            'template-curly-spacing',
+            'no-var',
+            'no-void',
+            'object-curly-spacing',
+            'no-unneeded-ternary',
+            'object-shorthand',
+            'one-var',
+            'padding-line-between-statements',
+            'prefer-const',
+            'prefer-object-spread',
+            'quote-props',
+            'radix',
+            'space-before-function-paren',
+            'space-in-parens',
+            'use-isnan',
+            'valid-typeof',
+            'yoda',
+            'prefer-template',
+            'jsdoc/no-types',
         ])('should throw error for %s', ruleId => {
             expect(hasError(result, ruleId)).toBeTruthy();
         });
